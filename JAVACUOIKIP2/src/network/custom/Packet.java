e66d75136a328aab2347ac599c341e0dacc7ed66
babynghe2003 <himinhpho@gmail.com>
2022-07-06 18:42:00 +0700

    update

--- JAVACUOIKIP2/src/network/custom/Packet.java
+++
@@ -1,5 +0,0 @@
-package network.custom;
-
-public class Packet {
-
-}
--- JAVACUOIKIP2/src/network/custom/cpacket/CPacketJoin.java
+++
@@ -1,7 +0,0 @@
-package network.custom.cpacket;
-
-import network.custom.Packet;
-
-public class CPacketJoin extends Packet{
-	
-}
